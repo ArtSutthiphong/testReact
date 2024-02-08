7c5da7b94f5918808c734770224805100d6771bd
ArtSutthiphong <71722477+ArtSutthiphong@users.noreply.github.com>
2024-02-08 16:29:22 +0700

    first commit test on branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import a from './favicon.ico'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={a} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Vue
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
